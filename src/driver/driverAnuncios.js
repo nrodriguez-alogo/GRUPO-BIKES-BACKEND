2676438f42725fbe19e108d6fa35d3b314365d6e
diego <deathgalvis@gmail.com>
2026-09-14 23:02:58 -0500

    driver anuncios, crear anuncios

--- src/driver/driverAnuncios.js
+++ src/driver/driverAnuncios.js
@@ -1,21 +1,37 @@
-import modeloNoticias from "../modelos/modeloNoticias";
+import {uploadImage} from '../middlewares/resources.js';
+import modeloNoticias from "../modelos/modeloNoticias.js";
 
 const controladorAnuncios = {
-    crearAnuncio : async (req, res)=>{
+    crearAnuncio: async (req, res)=>{
         try {
-            const nuevoAnuncio = new modeloNoticias({
+            uploadImage (req, res , async (error)=>{
+                if(error){
+                    return res.json({
+                        mensaje: 'Ocurrió un error al cargar la imagen',
+                        datos: error
+                    });
+               }
+               const nuevoAnuncio = new modeloNoticias({
                     titulo:req.body.nombre,
                     subtitulo:req.body.subtitulo,
                     contenido:req.body.contenido,
                     autor:req.body.autor,
                     imagen:req.file.filename
                 });
+                const guardarAnuncio = await nuevoAnuncio.save();
 
+                res.json ({
+                    mensaje:'Nuevo anuncio creado satisfactoriamente',
+                    datos: guardarAnuncio
+                });
+            });
+        } catch (error) {
+              res.json ({
+                    mensaje:'Ocurrio un error al crear el anuncio',
+                    datos: error
+                }); 
         }
+    }
+}
 
-
-/*titulo: { type: String, required: true, trim: true },
-    subtitulo: { type: String, required: true, trim: true },
-    contenido: { type: String, required: true, trim: true },
-    imagenNoticias: { type: String, required: true, },
-    autor: { type: String, 
+        export default controladorAnuncios;
